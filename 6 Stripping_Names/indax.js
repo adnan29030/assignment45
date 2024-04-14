d82f8752ea264fabbfe1742bbab20221fa8be5fa
adnan29030 <smg29030@gmail.com>
2024-04-13 23:06:27 -0700

    first commit

---
+++ 6 Stripping_Names/indax.js
@@ -0,0 +1,7 @@
+//----------------------Stripping Names ----------------------------
+let student_GH = "\n \t Adnan Ali Khan \n \t";
+//-----------------------Name with white spacing -------------------
+console.log(student_GH);
+//-----------------------Name wihtout white spacing ----------------
+console.log(student_GH.trim());
+export {};
---
+++ 6 Stripping_Names/indax.ts
@@ -0,0 +1,12 @@
+
+//----------------------Stripping Names ----------------------------
+
+let student_GH : string = "\n \t Adnan Ali Khan \n \t"
+
+//-----------------------Name with white spacing -------------------
+
+console.log (student_GH);
+
+//-----------------------Name wihtout white spacing ----------------
+
+console.log(student_GH.trim());
