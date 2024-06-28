b02fd86a6285ce3ea1b1ad3d9b115816231bdfee
adnan29030 <smg29030@gmail.com>
2024-06-28 12:37:24 -0700

    first commit

---
+++ 12 Greeting/indax.ts
@@ -0,0 +1,14 @@
+
+//----------Question 12 --------------------------
+//-------------------Message---------------------
+
+let message : string = " whould you like to meet Mr imran Khan? "
+
+//----------------Guest List----------------------
+let names: string[] = ["Farhan", "Noman", "Moiz", "Maryam"]
+  for(let i= 0; i< names.length; i++){
+    console.log(names[i] +  message)
+  }
+
+//---------------print /output------------------
+  
