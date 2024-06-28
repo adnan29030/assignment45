83dc090734bb60d83a1375ee3ee13f9d61420725
adnan29030 <smg29030@gmail.com>
2024-06-28 09:36:18 -0700

    first commit

---
+++ 4 Famous_Quote1/app.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+// -------------- Question No 4 Famous Quote Part 1 -----------------------------
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("\n \t Albert Einstein once said, “A person who never made a mistake never tried anything new.”\n \t ");
+//----------- output display on command prompt ------------------------
---
+++ 4 Famous_Quote1/app.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// -------------- Question No 4 Famous Quote Part 1 -----------------------------
+
+console.log("\n \t Albert Einstein once said, “A person who never made a mistake never tried anything new.”\n \t ");
+
+//----------- output display on command prompt ------------------------
