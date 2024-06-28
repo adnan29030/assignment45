131c97901201d473f31e771b9db10d61ce2768c8
adnan29030 <smg29030@gmail.com>
2024-06-28 12:25:46 -0700

    first commit

---
+++ 9 Favorite_Number/indax.js
@@ -0,0 +1,8 @@
+//------------------Favorit Number--------------------
+let my_favoritNumber = 8;
+let message = `${my_favoritNumber} is my favorit number.`;
+let message1 = "Here is my favorit number " + my_favoritNumber;
+//-------------------Print-------------------------
+console.log(message);
+console.log(message1);
+export {};
